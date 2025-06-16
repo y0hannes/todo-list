202d85c4078191e16a796a3f45dbb6e7cc00f559
y0hannes <yohanneset3@gmail.com>
2025-06-16 20:58:08 +0300

    Add task complete check

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import './view.css'
 
 const View = () => {
     const [tasks, setTasks] = useState([])
@@ -7,23 +8,48 @@ const View = () => {
     const AddTask = (e) => {
         e.preventDefault()
         if (newTask.trim() != ""){
-            setTasks([...tasks, newTask])
+            const task = {
+                text: newTask,
+                isCompleted: false
+                }
+            setTasks([...tasks, task])
             setNewTask("")
         }
     }
-
+    
+    const changeStatus = (index) => {
+        const updated = [...tasks]
+        updated[index].isCompleted = !updated[index].isCompleted
+        setTasks(updated)
+    }
+    
     return (
         <>
-            <form action="">
+            <form>
                 <input type="text" placeholder="Add task" value={newTask} onChange={(e) => setNewTask(e.target.value)}>
                 </input>
                 <button onClick={ AddTask }> Add </button>
             </form>
 
-            <p> List of tasks </p>
+            <p> List of Tasks </p>
             <ul>
-                {tasks.map((task, index) => (<li key={index}> {task} </li>))}
+                {tasks.map((task, index) =>
+                    !task.isCompleted ? (
+                        <li key={index}>
+                            {task.text}
+                            <input type="checkbox" checked={task.isCompleted} onChange={() => changeStatus(index)} />
+                        </li>
+                    ) : null
+                )}
             </ul>
+
+            <p>Completed Tasks</p>
+            <ul>
+                {tasks.map((task, index) =>
+                    task.isCompleted ? <li key={index}>{task.text}</li> : null
+                )}
+            </ul>
+
         </>
     )
 }
